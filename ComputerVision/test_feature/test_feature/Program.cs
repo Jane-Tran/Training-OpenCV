773e5625c57dcbfd0f80ef57024ec1f12489ff75
Jane-Tran <quanghuy2621998@gmail.com>
2020-10-16 17:37:13 +0700

    test build file dll

---
+++ ComputerVision/test_feature/test_feature/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test_feature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
