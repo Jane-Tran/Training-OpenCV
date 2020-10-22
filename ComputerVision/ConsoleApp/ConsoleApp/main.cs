60834350c2f8ab1b56c4b2dd171919a60d35e944
Jane-Tran <quanghuy2621998@gmail.com>
2020-10-22 18:16:55 +0700

    test Asynchronous C#

--- ComputerVision/ConsoleApp/ConsoleApp/main.cs
+++ ComputerVision/ConsoleApp/ConsoleApp/main.cs
@@ -1,37 +1,97 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using Cshape_Wrapper;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections;
 
 namespace ConsoleApp
 {
     class main
     {
-/*        [DllImport("fpga.dll", EntryPoint = "fibonacci_init", CallingConvention = CallingConvention.Cdecl)]
-        static extern void fibonacci_init(long a, long b);
-
-        [DllImport("fpga.dll", EntryPoint = "fibonacci_next", CallingConvention = CallingConvention.Cdecl)]
-        static extern bool fibonacci_next();
-
-        [DllImport("fpga.dll", EntryPoint = "fibonacci_current", CallingConvention = CallingConvention.Cdecl)]
-        static extern long fibonacci_current();
-
-        [DllImport("fpga.dll", EntryPoint = "fibonacci_index", CallingConvention = CallingConvention.Cdecl)]
-        static extern int fibonacci_index();*/
-
+        public static bool flag = true;
+        public static List<int> list;
         static void Main(string[] args)
         {
-            
-            Program.Fibonacci_init(1, 1);
+
+*//*            Program.Fibonacci_init(1, 1);
             do
             {
                 Console.WriteLine(Program.Fibonacci_current());
 
             } while (Program.Fibonacci_next() && Program.Fibonacci_current() > 0);
-            Console.ReadLine();
+            Console.ReadLine();*//*
+
+            list = new List<int>();
+
+            Console.WriteLine("Thead Vs Async/Await");
+
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
+            var task_one = TaskOne();
+            var task_two = TaskTwo();
+
+            Task.WaitAll(task_one, task_two);
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+
+            Console.ReadKey();
+        }
+
+        private static async Task TaskOne()
+        {
+            //await Task.Delay(300);
+            int i = 0;
+            while (flag)
+            {
+                i++;
+                await Task.Delay(300);
+                Console.WriteLine("TaskOne run -> " + i);
+                list.Add(i);
+                *//*list.ForEach(i =>
+                {
+                    Console.WriteLine("index:" + list.IndexOf(i) + " - value: " + i);
+                });*//*
+
+                Console.WriteLine(list[list.Count-1]);
+                if (i == 15) break;
+            }
+        }
+
+        private static async Task TaskTwo()
+        {
+            *//*await Task.Delay(1000);
+            int total = 0;
+            Console.WriteLine("TaskTwo -> Total " + total);
+            int i = 0;
+            while (true)
+            {
+                await Task.Delay(1);
+                total += list[i];
+                list.RemoveAt(0);
+                if (i == 14) break;
+                Console.WriteLine("TaskTwo -> Total " + total);
+            }*//*
+
+            while (true)
+            {
+                await Task.Delay(310);
+                list.ForEach(i =>
+                {
+                    Console.WriteLine("index:" + list.IndexOf(i) + " - value: " + i);
+                });
+                Console.WriteLine(list.Sum());
+            }
+            
+            *//*await Task.Delay(300);
+            Console.WriteLine("hi2");
+            await Task.Delay(4400);
+            Console.WriteLine("Canel, Total" + list.Sum());
+            flag = false;*//*
         }
     }
 }
+*/
---
+++ ComputerVision/ConsoleApp/ConsoleApp/thread.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApp
+{
+    class thread
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Thead Vs Async/Await");
+
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
+            // sử dụng Thread để lập trình bất đồng bộ
+            Thread th_one = new Thread(ThreadOne);
+            Thread th_two = new Thread(ThreadTwo);
+
+            th_one.Start();
+            th_two.Start();
+
+            // Chặn luồng tiếp tục cho tới khi các tiến trình th_one và th_two hoàn thành
+            th_one.Join();
+            th_two.Join();
+
+            watch.Stop();
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+
+            /*List<int> list = new List<int> { 1, 2, 3, 4, 5 };
+
+            list.ForEach(i =>
+            {
+                Console.WriteLine("index:" + list.IndexOf(i) + " - value: " + i);
+            });
+
+            int result = list[0] + list[1];
+            Console.WriteLine(result);
+
+            list.RemoveAt(1);
+            list.RemoveAt(0);
+
+            list.ForEach(i =>
+            {
+                Console.WriteLine("index:" + list.IndexOf(i) + " - value: " + i);
+            });
+
+
+            while (list.Count > 0)
+            {
+                result += list[0];
+                list.RemoveAt(0);
+            }
+
+            Console.WriteLine(result);
+            list.ForEach(i =>
+            {
+                Console.WriteLine("index:" + list.IndexOf(i) + " - value: " + i);
+            });*/
+
+            Console.ReadKey();
+        }
+
+        private static void ThreadOne()
+        {
+            Thread.Sleep(5000);
+            Console.WriteLine("ThreadOne");
+        }
+
+        private static void ThreadTwo()
+        {
+            Thread.Sleep(2000);
+            Console.WriteLine("ThreadTwo");
+            Thread.Sleep(1000);
+            Console.WriteLine("hi 1s");
+        }
+    }
+}
